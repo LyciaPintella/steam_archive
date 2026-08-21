548c80b25bfbb49d45eb9fb5852bf87e4f0b026c
Jessica Murphy <jessica.ariadne.murphy@gmail.com>
2026-08-21 08:27:34 -0400

    first commit

---
+++ archive/ConfigModel.cs 2026-08-20 055255.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace steam_tools
+{
+    public class ConfigModel
+    {
+        public List<string> steamLibs { get; set; }
+    }
+}
